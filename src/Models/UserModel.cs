3779f556296a1fd048a2661636962b15b7fe654d
Menyhárt Attila <menyhart.atika@gmail.com>
2023-03-21 17:48:55 +0100

    User Model

---
+++ src/Models/UserModel.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDbGenericRepository.Attributes;
+
+namespace ProgramTan.WebApi.Models;
+
+[CollectionName("users")]
+public class UserModel
+{
+	[BsonId]
+	[BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+	public string Id { get; init; }
+
+	[Required]
+	[BsonElement("username")]
+	public string Username { get; init; }
+
+	[Required]
+	[BsonElement("password")]
+	public string Password { get; init; }
+}

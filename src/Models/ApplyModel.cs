7ecf4f34b5ea3f2823ff8b78fadfb0df217b07f6
Menyhárt Attila <menyhart.atika@gmail.com>
2023-04-08 14:57:45 +0200

    ApplyModel

---
+++ src/Models/ApplyModel.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDbGenericRepository.Attributes;
+
+namespace ProgramTan.WebApi.Models;
+
+[CollectionName("users")]
+public class ApplyModel : IEntity
+{
+	[BsonId]
+	[BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+	public string Id { get; init; }
+
+	[Required]
+	[BsonElement("email")]
+	public string Email { get; set; }
+
+	[BsonElement("phone")]
+	public string Phone { get; set; }
+
+	[Required]
+	[BsonElement("github")]
+	public string Github { get; set; }
+}

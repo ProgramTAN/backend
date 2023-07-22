f0f9a4bf570e5dfc202bc9e120d9ae30ffde0925
Menyhárt Attila <menyhart.atika@gmail.com>
2023-07-22 17:52:36 +0200

    GetGuildById method

--- src/Services/GuildService.cs
+++ src/Services/GuildService.cs
@@ -8,15 +8,12 @@ public class GuildService : ServiceBase<GuildModel>
 {
 	public GuildService(MongoDbConfig config) : base(config) { }
 
-	public async Task<GuildModel> CreateGuildAsync(ulong guildId)
-	{
-		var guild = new GuildModel
-		{
-			GuildId = guildId,
-			Settings = new(),
-			Classes = new()
-		};
+	public async Task<GuildModel> GetByGuildIdAsync(ulong guildId) =>
+		await Collection.Find(g => g.GuildId == guildId)
+			.FirstOrDefaultAsync();
 
+	public async Task<GuildModel> CreateGuildAsync(GuildModel guild)
+	{
 		await Collection.InsertOneAsync(guild);
 		return guild;
 	}
